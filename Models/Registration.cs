a90bf30de74bb6d4977d6a00c680ab63b12ab932
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-10-15 18:20:29 +0530

    Add project files.

---
+++ Models/Registration.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace ASP_First.Models
+{
+    public partial class Registration
+    {
+        [Key]
+        
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Enter the username")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "Enter the useremail")]
+        public string? Useremail { get; set; }
+        [Required(ErrorMessage = "Enter the password")]
+        public string? Password { get; set; }
+        [Required(ErrorMessage = "Enter the mobile number")]
+        public string? Mobile { get; set; }
+    }
+}
